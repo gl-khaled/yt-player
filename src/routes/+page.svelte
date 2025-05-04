dbc711d9d48ca80ed4dbcd35ff9464d7f2f65940
khaled <khaledghariblatrach@gmail.com>
2025-05-04 18:42:13 +0100

    add download

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { YtDlp } from "ytdlp-nodejs";
+
+  const ytdlp = new YtDlp();
+  async function downloadVideo() {
+    try {
+      const output = await ytdlp.downloadAsync(
+        "https://www.youtube.com/watch?v=_AL4IwHuHlY",
+        {
+          onProgress: (progress) => {
+            console.log(progress);
+          },
+          // others args
+        },
+      );
+      console.log("Download completed:", output);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  downloadVideo();
+</script>
